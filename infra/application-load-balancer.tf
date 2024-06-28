aadf01d7917163e73587b0b94c2f9b1fc1f1ba7b
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-06-28 20:53:37 -0300

    infra: remove acesso no array do data

--- infra/application-load-balancer.tf
+++ infra/application-load-balancer.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "target-group-cluster-fiap" {
 resource "aws_lb_target_group_attachment" "attach" {
   depends_on = [data.aws_instance.ec2]
   target_group_arn = aws_lb_target_group.target-group-cluster-fiap.arn
-  target_id        = data.aws_instance.ec2[0].id
+  target_id        = data.aws_instance.ec2.id
   port             = 80
 }
 
